eee98d29f8616f99c51255533ac6a33c2a348161
Daniel Skonieczny <danielskonieczny@Mac-mini-Daniel.local>
2025-07-21 18:56:04 +0200

    fix: remove Navigation

--- src/app/components/Layout.tsx
+++ src/app/components/Layout.tsx
@@ -1,10 +1,8 @@
-import Navigation from './Navigation'
 import type { ReactNode } from 'react'
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <>
-      <Navigation />
       <main className="max-w-4xl mx-auto px-4 py-8">{children}</main>
     </>
   )
